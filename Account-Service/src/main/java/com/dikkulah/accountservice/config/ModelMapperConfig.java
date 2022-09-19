1a7c240afae8e9c4f10543413ef2182ed65b38e9
dikkulah <dikkulah@gmail.com>
2022-09-20 00:05:25 +0300

    Sonarlint auto analyze stopped, ModelMapper added to backend-service, Dto refactoring started.

---
+++ Account-Service/src/main/java/com/dikkulah/accountservice/config/ModelMapperConfig.java
@@ -0,0 +1,23 @@
+package com.dikkulah.accountservice.config;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper modelMapper(){
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        modelMapper.getConfiguration().setSkipNullEnabled(true);
+        return modelMapper;
+    }
+     
+
+
+
+}
--- Account-Service/src/main/java/com/dikkulah/accountservice/repository/ActivitiesRepository.java
+++ Account-Service/src/main/java/com/dikkulah/accountservice/repository/ActivitiesRepository.java
@@ -1,6 +1,8 @@
 package com.dikkulah.accountservice.repository;
 
 import com.dikkulah.accountservice.model.Activity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -8,4 +10,6 @@ import java.util.UUID;
 
 public interface ActivitiesRepository extends JpaRepository<Activity, UUID> {
     List<Activity> findActivitiesByAccount_Id(UUID account_id);
+    Page<Activity> findActivitiesByAccount_IdOrderByTimeDesc(UUID account_id,
+                                                             Pageable pageable);
 }
